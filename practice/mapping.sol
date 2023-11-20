33520f0b011905d9d604d69622924bba59c59d3a
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-11-20 22:45:55 +0530

    mapping and structs

---
+++ practice/mapping.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+struct User {
+    uint roll;
+    string name;
+}
+
+contract MappingContract {
+    mapping(uint => User) public roll_to_name;
+    User public bharat;
+
+    constructor(uint roll, string memory name) {
+        bharat.roll = roll;
+        bharat.name = name;
+    }
+
+    function setter(uint roll) public {
+        roll_to_name[roll] = bharat;
+    }
+}
