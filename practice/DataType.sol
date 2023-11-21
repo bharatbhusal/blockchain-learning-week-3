5b2ec615bc7d31ac4707debabc3d8abde39755df
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-11-21 22:51:20 +0530

    overflow demo

---
+++ practice/DataType.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+contract DataTypes{
+    int8 num;
+}
---
+++ practice/Overflow.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.5.0;
+
+contract Overflow{
+    uint8 public money = 255;
+
+    function setter() public{
+        money += 1;
+    }
+}
