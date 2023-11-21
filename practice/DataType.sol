99d867776c4499c42c9f214820c49bc666b9c711
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-11-21 23:13:36 +0530

    array

--- practice/DataType.sol
+++ practice/DataType.sol
@@ -2,5 +2,13 @@
 pragma solidity 0.8.23;
 
 contract DataTypes{
-    int8 num;
+    uint8[] public my_array = [1, 2, 3, 4, 5, 6];
+
+function setter(uint8 index, uint8 value) public {
+    my_array[index]= value;
+}
+
+function length() public view returns(uint){
+    return my_array.length;
+}
 }
