3fe9214259c3847cf14055e1e2e7e339879ec44a
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-11-21 23:52:46 +0530

    conditionals

---
+++ practice/Conditionals.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+contract Conditionals{
+
+    function demo(uint8 day_number) public  pure returns(string memory){
+        if (day_number == 0){
+            return "Sunday";
+        } else if (day_number == 1){
+            return  "Monday";
+        } else if (day_number == 2){
+            return  "Tuesday";
+        } else if (day_number == 3){
+            return  "Wednesday";
+        } else if (day_number == 4){
+            return  "Thrusday";
+        } else if (day_number == 5){
+            return  "Friday";
+        } else if (day_number == 6){
+            return "Saturday";
+        } else {
+            return "Invalid Day number!!";
+        }
+    }
+}
