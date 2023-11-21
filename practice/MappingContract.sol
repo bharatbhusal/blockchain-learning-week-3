0d01e65e5dfc89e6e360e13a5dac684c7f8ef50e
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-11-22 00:46:09 +0530

    mapping

--- practice/MappingContract.sol
+++ practice/MappingContract.sol
@@ -1,21 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-struct User {
-    uint roll;
-    string name;
-}
+
 
 contract MappingContract {
-    mapping(uint => User) public roll_to_name;
-    User public bharat;
 
-    constructor(uint roll, string memory name) {
-        bharat.roll = roll;
-        bharat.name = name;
+    enum branch{
+        CSE,
+        CS,
+        AI_ML,
+        IOT
     }
 
-    function setter(uint roll) public {
-        roll_to_name[roll] = bharat;
+    enum rollInitialsType{
+        HU21,
+        HU22,
+        VU21,
+        VU22
+    }
+
+    struct User {
+        uint class_roll;
+        string name;
+        branch course_branch;
+        rollInitialsType rollInitials;
+    }
+
+    mapping(uint => User) public roll_to_name;
+    User public student;
+
+    constructor(uint roll, string memory name) {
+        student.class_roll = roll;
+        student.name = name;
+        student.course_branch = branch.CS;
+        student.rollInitials = rollInitialsType.HU21;
     }
 }
