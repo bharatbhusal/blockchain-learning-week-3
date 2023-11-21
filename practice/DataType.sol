609b552939bd7e6c255ba88fe66cfd4a4d0d83e4
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-11-21 23:19:00 +0530

    dynamic array operations

--- practice/DataType.sol
+++ practice/DataType.sol
@@ -2,13 +2,26 @@
 pragma solidity 0.8.23;
 
 contract DataTypes{
-    uint8[] public my_array = [1, 2, 3, 4, 5, 6];
+    uint8[6] public my_array = [1, 2, 3, 4, 5, 6];
+    uint[] public my_dynamic_array;
 
-function setter(uint8 index, uint8 value) public {
-    my_array[index]= value;
-}
+    function setter(uint8 index, uint8 value) public {
+        my_array[index]= value;
+    }
 
-function length() public view returns(uint){
-    return my_array.length;
-}
+    function length() public view returns(uint){
+        return my_array.length;
+    }
+
+    function push(uint value) public {
+        my_dynamic_array.push(value);
+    }
+
+    function pop() public {
+        my_dynamic_array.pop();
+    }
+
+    function len() public view returns(uint){
+        return  my_dynamic_array.length;
+    }
 }
