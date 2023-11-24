deb43a201440b6f523e181b88d1878c677a995bb
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-11-24 23:10:29 +0530

    event and emit

--- practice/Logging.sol
+++ practice/Logging.sol
@@ -2,5 +2,14 @@
 pragma solidity 0.8.23;
 
 contract Logging{
-    
+    event eventDemo(
+        uint    roll,
+        uint age,
+        string  name,
+        string indexed last
+    );
+
+    function callEmmit() public {
+        emit eventDemo(45, 33, "bharat", "bhusal");
+    }
 }
