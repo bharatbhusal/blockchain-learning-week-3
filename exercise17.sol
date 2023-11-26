33fc4d38a2e9190f9c0ad506458ae1cf940e5101
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-11-26 21:01:31 +0530

    lock mechanism

--- exercise17.sol
+++ exercise17.sol
@@ -13,10 +13,12 @@ contract TokenBharat {
     // balance of the given address. balance getter function.
     mapping(address => uint256) public balances;
 
+    //modifier for features that only owner can run.
     modifier onlyOwner {
         require(msg.sender == owner, "Only owner is allowed to use this feature");
         _;
     }
+
     constructor(
         string memory _name,
         string memory _symbol,
@@ -45,7 +47,7 @@ contract TokenBharat {
 
   
     // function to mint the tokens in own wallet.
-    function minttoOwner(uint256 amount) public onlyOwner {
+    function mintToOwner(uint256 amount) public onlyOwner {
 
         require((totalSupply + amount) <= maxSupply, "Max supply exceeding");
         //adding the balance to owner's wallet and increasing the supply.
@@ -65,17 +67,20 @@ contract TokenBharat {
 }
 
 contract AdvancedTokenBharat is TokenBharat{
-
-
+    //event to log each lock item.
+    event logLock(uint256, uint256, uint256);
+    
+    //Lock iteam struct
     struct Lock{
         uint256 amount;
+        uint256 lockedTimeStamp;
         uint256 unlockTimeStamp;
     }
 
-    mapping(address => mapping(uint256 => Lock)) private lockPeriodAndAmount; //first uint lockPeriod, second uint amount.
+    //user address mapping to the tuple of Locks. one user can have multiple lock duration for varying number of tokens.
+    mapping(address =>  Lock[]) public lockPeriodAndAmount;
 
-    constructor() TokenBharat("AdvancedTokenBharat", "ABHT", 18, 500*(10**18), 5000*(10**18)){
-    }
+    constructor() TokenBharat("AdvancedTokenBharat", "ABHT", 18, 500*(10**18), 5000*(10**18)){}
 
     //To mint tokens in desired user wallet by owner
     function mintToUser(address user, uint256 amount) public onlyOwner {
@@ -85,17 +90,21 @@ contract AdvancedTokenBharat is TokenBharat{
         totalSupply += amount;
     }
 
+
     //To burn token from own wallet by any user
     function burn(uint256 amount) public {
+        //subtracting locked tokens while comparision.
         require(balances[msg.sender] - numberOfTokensLocked(msg.sender) >= amount, "Insufficient balance");
+        
         //subtracting balance from owner's wallet and total supply
         balances[msg.sender] -= amount;
         totalSupply -= amount;
     }
 
     function tranfer(uint256 amount, address to) public override {
-        //the receiving wallet should be valid and sender should have enough balance.
+        //Sender should have enough balance. subtracting locked tokens while comparision.
         require(balances[msg.sender] - numberOfTokensLocked(msg.sender) >= amount, "Insufficient balance");
+        // the receiving wallet should be valid 
         require(to != address(0), "Invalid address");
 
         //subtracting balance from sender and adding in receiver.
@@ -103,20 +112,28 @@ contract AdvancedTokenBharat is TokenBharat{
         balances[to] += amount;
     }
 
+    //Funtion to lock tokens of any user by owner of contract.
     function lockToken(uint256 lockDuration, uint256 amount, address user) public onlyOwner{
         require(amount > 0, "Can't lock 0 tokens");
         require(balances[user] >= amount, "Insufficient balance");
         require(lockDuration > 0, "Invalid lockDuration");
         require(user != address(0), "Invalid User address");
+
         uint256 unlockTimeStamp = block.timestamp + lockDuration;
-        Lock memory newLock = Lock(amount, unlockTimeStamp);
-        lockPeriodAndAmount[user][unlockTimeStamp] = newLock;
+        Lock memory newLock = Lock(amount, block.timestamp, unlockTimeStamp);
+        emit logLock(newLock.amount, newLock.lockedTimeStamp, newLock.unlockTimeStamp);
+        
+        lockPeriodAndAmount[user].push(newLock);
     }
 
-    function numberOfTokensLocked(address user) public view returns (uint256){
-        if (lockPeriodAndAmount[user][block.timestamp].unlockTimeStamp >= block.timestamp){
-            return lockPeriodAndAmount[user][block.timestamp].amount;
-        } else 
+    //function to return number of tokens locked of a user.
+    function numberOfTokensLocked(address user) private view returns (uint256){
+        Lock[] memory userLock = lockPeriodAndAmount[user];
+        for (uint8 i = 0; i < userLock.length;i++){
+            if (userLock[i].unlockTimeStamp >= block.timestamp){
+                return (userLock[i].amount);
+            }
+        }
         return 0;
     }
 }
