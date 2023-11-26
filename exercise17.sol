e40a21597827667869acebdca214a4a805210c6f
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-11-26 16:22:38 +0530

    lock mechanism

--- exercise17.sol
+++ exercise17.sol
@@ -54,7 +54,7 @@ contract TokenBharat {
     }
 
     //function to transfer the tokens from one wallet to another.
-    function tranfer(uint256 amount, address to) public {
+    function tranfer(uint256 amount, address to) public virtual {
         //the receiving wallet should be valid and sender should have enough balance.
         require(balances[msg.sender] >= amount, "Insufficient balance");
         require(to != address(0), "Invalid address");
@@ -65,8 +65,16 @@ contract TokenBharat {
 }
 
 contract AdvancedTokenBharat is TokenBharat{
+
+
+    struct Lock{
+        uint256 amount;
+        uint256 unlockTimeStamp;
+    }
+
+    mapping(address => mapping(uint256 => Lock)) private lockPeriodAndAmount; //first uint lockPeriod, second uint amount.
+
     constructor() TokenBharat("AdvancedTokenBharat", "ABHT", 18, 500*(10**18), 5000*(10**18)){
-        
     }
 
     //To mint tokens in desired user wallet by owner
@@ -79,10 +87,36 @@ contract AdvancedTokenBharat is TokenBharat{
 
     //To burn token from own wallet by any user
     function burn(uint256 amount) public {
-        require(balances[msg.sender] >= amount, "Insufficient balance");
-
+        require(balances[msg.sender] - numberOfTokensLocked(msg.sender) >= amount, "Insufficient balance");
         //subtracting balance from owner's wallet and total supply
         balances[msg.sender] -= amount;
         totalSupply -= amount;
+    }
+
+    function tranfer(uint256 amount, address to) public override {
+        //the receiving wallet should be valid and sender should have enough balance.
+        require(balances[msg.sender] - numberOfTokensLocked(msg.sender) >= amount, "Insufficient balance");
+        require(to != address(0), "Invalid address");
+
+        //subtracting balance from sender and adding in receiver.
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function lockToken(uint256 lockDuration, uint256 amount, address user) public onlyOwner{
+        require(amount > 0, "Can't lock 0 tokens");
+        require(balances[user] >= amount, "Insufficient balance");
+        require(lockDuration > 0, "Invalid lockDuration");
+        require(user != address(0), "Invalid User address");
+        uint256 unlockTimeStamp = block.timestamp + lockDuration;
+        Lock memory newLock = Lock(amount, unlockTimeStamp);
+        lockPeriodAndAmount[user][unlockTimeStamp] = newLock;
+    }
+
+    function numberOfTokensLocked(address user) public view returns (uint256){
+        if (lockPeriodAndAmount[user][block.timestamp].unlockTimeStamp >= block.timestamp){
+            return lockPeriodAndAmount[user][block.timestamp].amount;
+        } else 
+        return 0;
     }
 }
